2aaec0488e05446d869a49e4b11df44dd7aa5efd
enrique ramirez <you@example.com>
2019-08-30 11:22:09 -0500

    version 1 prototipo funcional

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+ const config = {
+    apiKey: "AIzaSyBur2M7rsHmWBpSmFdCjNpSkOitlfyTqNg",
+    authDomain: "socialapp-83f33.firebaseapp.com",
+    databaseURL: "https://socialapp-83f33.firebaseio.com",
+    projectId: "socialapp-83f33",
+    storageBucket: "socialapp-83f33.appspot.com",
+    messagingSenderId: "529252654957"
+  }
+
+  export default config
---
+++ src/app/profile/profile.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage implements OnInit {
+
+  userPosts
+
+  constructor(private afs: AngularFirestore, private user: UserService, private router: Router) { 
+        const posts = afs.doc(`users/${user.getUID()}`)
+        this.userPosts = posts.valueChanges()
+      }
+
+      goTo(postID: string) {
+        this.router.navigate(['tabs/post/' + postID.split('/')[0]])
+
+      }
+  ngOnInit() {
+  }
+
+}
